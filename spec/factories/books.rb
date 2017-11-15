8ab64c0e47e4d0b9db97ec8e4f236e198ee704a7
dracma_bucket <macrodracma@gmail.com>
2017-11-14 22:47:47 -0600

    book factory fixed

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :book do
     name { Faker::Name.name }
     author { Faker::Name.name }
-    user
+    user_id nil
     status 'available'
     after(:build) {|book|  book.categories = [create(:category)]}
    
